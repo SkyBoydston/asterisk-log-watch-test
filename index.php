aea04d0ce611a0c3ed36afcc7194ff7694cab833
SkyBoydston <sky.boydston@gmail.com>
2016-08-21 19:21:15 -0600

    preparing for major refactoring, maybe unstable

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
 
-// Notes
-// What if there's only one ip and time? need to take care of sits when you're not dealing with arrays or of length one.
-// What if no placeholder exists?
-
-
-
-
 /* This is a program that identifies attempts
  * to access a server without permission and 
  * notifies the server owner with the offenders'
@@ -14,59 +7,48 @@
  * Cron job.
 */ 
 
-error_reporting(E_ALL);
 
 // Open the log and pare it down to only relevant entries.
 
-$log = file_get_contents('log.txt');
+if (file_exists('log.txt')) {
+	$log = file_get_contents('log.txt');
+} elseif (file_exists('full.1')) {
+	$log = file_get_contents('full.1');
+} else {
+	mail('kevin@networxonline.com', 
+		'Log file absence warning', 
+		'Please make sure that a log file that tracks unpermitted attempts to log into host PBX-2 is being generated.', 
+		$from);
+}
 
 if (strlen($log) >= 100000000) {  // This is here so that pcre.backtrack_limit doesn't need to be set by variable, which could result in a very large backtrack limit.
 	mail('kevin@networxonline.com', 
 		'Log file size warning', 
-		'Please make sure that log files do not exceed 100 million characters in length for optimal performance.', 
+		'Please make sure that log files to track unpermitted attempts to log into host PBX-2 do not exceed 100 million characters in length for optimal performance.', 
 		$from);
 }
 
 if (file_exists('log_placeholder.txt') && strlen($log) <= 100000000) {
-	$placeholder = file_get_contents('log_placeholder.txt', false, null, 0, 19); // Making sure it doesn't pick up an extra newline.
+	$placeholder = file_get_contents('log_placeholder.txt', false, null, 0, 19); // Making sure it doesn't pick up an extra newline or anything else.
 	$pattern = "/.*" . str_replace(' ', '\s', $placeholder) . "/s";
-	// $pattern = htmlentities($pattern);
-
-	// echo 'placeholder is ' . $placeholder;
-	// echo '<br>';
-	// echo 'pattern is ' . $pattern;
-	// echo '<br>';
-
-	$count = true;
 	ini_set('pcre.backtrack_limit', '100000000');
-	$log = preg_replace($pattern, '', $log, -1, $count);
-
-	// echo 'count is ' . $count . '<br>';
-	echo 'log size is ' . strlen($log);
-	echo '<br>';
-	// echo 'error was ' . preg_last_error() . '<br>';
-	// echo array_flip(get_defined_constants(true)['pcre'])[preg_last_error()];
-	// echo '<br>';
-	// echo substr($log, 0, 1000);
-
-
+	$log = preg_replace($pattern, '', $log);
 } else {
 	$placeholder = 0;
 }
+
+
+
+
+
+
+
 
 
 // Get together a list of notices.
 
 $notices = array();
 preg_match_all('/\n.*NOTICE\[23697\].*\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}.*\n/', $log, $notices); // This needs to be changed for the final version
-
-// echo '<pre>';
-// for($i=0;$i<=30;$i++) {
-// 	echo $notices[0][$i];
-// 	echo '<br>';
-// // // 	var_dump($notices[$i]);
-// }
-// echo '</pre>';
 
 if (!empty($notices)) {
 	$notices = $notices[0];  // Not necessary but convenient because of the way preg_match_all() outputs its results.
@@ -91,24 +73,14 @@ if (!empty($notices)) {
 		array_push($suspect_ips, $ip);
 	}
 
-	// $suspect_ips = array_unique($suspect_ips);  // Cutting down the amount of data to work on.
-	// echo '<pre>suspect ips sorted normal<br>';
-	// 	var_dump($suspect_ips);
-	// echo '</pre>';
-
 	function array_unique_right($array)
 	{
 	  return array_reverse(array_unique(array_reverse($array, true)), true);
 	}
 
 	$suspect_ips = array_unique_right($suspect_ips);
-	echo '<pre>suspect ips sorted reverse<br>';
-		var_dump($suspect_ips);
-	echo '</pre>';
 
-	echo 'ips<br><pre>';
-	var_dump($suspect_ips);
-	echo '</pre>';
+
 
 
 
@@ -136,32 +108,14 @@ if (!empty($notices)) {
 	foreach ($suspect_ips as $key => $value) {
 		$times_of_suspect_ips[] = $times[$key];
 	}
-	echo 'times of suspect ips<br><pre>';
-	var_dump($times_of_suspect_ips);
-	echo '</pre>';
 
 	$time_of_latest_suspect = end($times_of_suspect_ips);
 	while (($time_of_latest_suspect == end($times_of_suspect_ips)) && (count($times_of_suspect_ips) > 1)) {
-		// echo 'touch count function';
 		array_pop($times_of_suspect_ips);
 	}
 
 
-
-	echo 'time of latest suspect <br>' . $time_of_latest_suspect;
-	echo '<br>';
-	echo 'times of suspect ips <br>';
-	var_dump($times_of_suspect_ips);
-	echo '<br>';
-
-
-
-
 	if (($time_of_latest_suspect != end($times_of_suspect_ips)) && (count($times_of_suspect_ips) > 1)){
-		echo 'touch';
-		echo '<br>';
-
-		array_pop($times_of_suspect_ips);  
 		$time_of_second_latest_suspect = end($times_of_suspect_ips);  
 	} elseif (($time_of_latest_suspect != end($times_of_suspect_ips)) && (count($times_of_suspect_ips) == 1)){
 		$time_of_second_latest_suspect = end($times_of_suspect_ips);  
@@ -169,11 +123,9 @@ if (!empty($notices)) {
 		$time_of_second_latest_suspect = null;
 	}
 
-	echo 'times of suspect ips after modification<br><pre>';
-	var_dump($times_of_suspect_ips);
-	echo 'time of second latest suspect<br><pre>';
-	var_dump($time_of_second_latest_suspect);
-	echo '</pre>';
+
+
+
 
 
 	// Store the latest suspect's time so for use on the next run of this script.
@@ -187,9 +139,6 @@ if (!empty($notices)) {
 	fclose($log_placeholder);
 		}
 	}
-
-
-
 
 
 
